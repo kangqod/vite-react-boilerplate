4bdff263e48fd6c3118ad86b1b7c778292aea418
byeongyong.kang <byeongyong.kang@vuno.co>
2021-07-22 18:30:03 +0900

    Add eslint, stylelint prettier husky lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
+import logo from '/logo.svg'
 import './App.css'
 
 function App() {
@@ -10,8 +10,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React</p>
+
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" 
+          onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
         </p>
@@ -19,12 +21,7 @@ function App() {
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
           {' | '}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  server: {
+    port: 3000,
+    open: true,
+    /*proxy: {
+      '/api': {
+        target: `http://localhost:${PORT}`,
+        changeOrigin: true,
+      },
+    }*/
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+});
